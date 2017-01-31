1b62a2bb415c993be738f74176efaedac5ccd0e7
tidusx18 <victorianowebdesign@gmail.com>
2017-01-31 16:10:32 -0500

    Replace URLs Initial Release
    Automatically replaces string/regex matches in TinyMCE editor HTML
    content and Web Link item URL fields (if present) with new string.

---
+++ replace-urls.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         Replace URLs
+// @namespace    github.com/tidusx18
+// @version      0.0.1
+// @description  Replaces all matching URLs in TinyMCE HTML content and Web Link URL field (if present) with new URL.
+// @author       Daniel Victoriano <victorianowebdesign@gmail.com>
+// @match        https://fiu.blackboard.com/*
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+if (unsafeWindow.tinyMCE !== undefined && document.body.className == "ineditmode") {
+  setTimeout(function() {
+
+    var oldUrl, newUrl, tinyMCE, tinyMCEId, tinyMCEHtml, webUrl, regex;
+
+    oldUrl = "http://old-url.com";
+    newUrl = "http://new-url.com";
+
+    // Get Editor instance by ID
+    // Editor ID varies so get ID first
+    tinyMCEId = document.querySelector(".textboxtable textarea").id;
+    tinyMCE = unsafeWindow.tinyMCE.getInstanceById(tinyMCEId);
+
+    // Get TinyMCE HTML content
+    tinyMCEHtml = tinyMCE.getContent({format: "html"});
+
+    // Get URL field (if present)
+    webUrl = document.getElementById("url");
+
+    // Replace URLs
+    regex = new RegExp(oldUrl, "g");
+
+    if ( tinyMCEHtml.includes(oldUrl) ) {
+      tinyMCEHtml = tinyMCEHtml.replace(regex, newUrl);
+    }
+
+    if (webUrl !== null) {
+      if ( url.includes(oldUrl) ) {
+        url = url.replace(regex, newUrl);
+      }
+    }
+
+    tinyMCE.setContent(tinyMCEHtml);
+  }, 2100);
+}
+})();
